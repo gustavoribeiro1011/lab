0ee4fbb200f4873189ed1dea226191609983e91c
Gustavo Ribeiro <gustavoribeiro1011@gmail.com>
2023-08-03 14:43:06 +0000

    lab

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,4 @@ Route::get('/contato', [SiteController::class, 'contato']);
 Route::get('/servicos', [SiteController::class, 'servicos']);
 Route::get('/servico/{id}', [SiteController::class, 'servico']);
 Route::get('/saudacao/{nome?}', Saudacao::class);
-Route::get('/dominios/{dominio}', [DominioController::class, 'verificarDominio']);
+Route::get('/dominios/{dominio?}', [DominioController::class, 'verificarDominio']);
