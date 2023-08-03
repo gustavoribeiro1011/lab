a59066d6902f2fdd51fbc0cc087df4a9c2d8c9bb
Gustavo Ribeiro <gustavoribeiro1011@gmail.com>
2023-08-03 15:35:42 +0000

    lab

--- app/Http/Controllers/Saudacao.php
+++ app/Http/Controllers/Saudacao.php
@@ -6,14 +6,14 @@ use Illuminate\Http\Request;
 
 class Saudacao extends Controller
 {
-/**
- * Undocumented function
- *
- * @param string $nome
- * @return void
- */
+    /**
+     * Undocumented function
+     *
+     * @param string $nome
+     * @return void
+     */
     public function __invoke(string $nome = "Treinaweb")
     {
-        echo "Ola $nome";
+        return view("saudacao")->with("nome", $nome);
     }
 }
