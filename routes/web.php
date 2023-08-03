5019b02a336d63ccf1ca24410a5ef9b68cc65321
Gustavo Ribeiro <gustavoribeiro1011@gmail.com>
2023-08-03 14:12:54 +0000

    lab

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\DominioController;
-use App\Http\Controllers\Dominios;
 use App\Http\Controllers\Saudacao;
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
@@ -27,4 +26,4 @@ Route::get('/contato', [SiteController::class, 'contato']);
 Route::get('/servicos', [SiteController::class, 'servicos']);
 Route::get('/servico/{id}', [SiteController::class, 'servico']);
 Route::get('/saudacao/{nome?}', Saudacao::class);
-Route::get('/dominios/{dominio}',[DominioController::class, 'verificarDominio']);
+Route::get('/dominios/{dominio}', [DominioController::class, 'verificarDominio']);
