fcbab457adab0ef1d0781ac254033c6c283394f6
Gustavo Ribeiro <gustavoribeiro1011@gmail.com>
2023-08-03 14:35:50 +0000

    lab

--- app/Http/Controllers/DominioController.php
+++ app/Http/Controllers/DominioController.php
@@ -6,8 +6,39 @@ use Illuminate\Http\Request;
 
 class DominioController extends Controller
 {
-    public function verificarDominio($dominio){
-        //
-        return view("dominio");
+    public function verificarDominio($dominio)
+    {
+
+        $listaDeDominios = [
+            1 => [
+                "dominio" => "alegria",
+                "status" => "ativo"
+            ],
+            2 => [
+                "dominio" => "seu_dominio",
+                "status" => "ativo"
+            ],
+            3 => [
+                "dominio" => "ciamilhas",
+                "status" => "ativo"
+            ]
+        ];
+
+        // Procurar o domínio no array de dados fictício
+        $status = null;
+        foreach ($listaDeDominios as $item) {
+            if ($item["dominio"] === $dominio) {
+                $status = $item["status"];
+                break;
+            }
+        }
+
+        // Caso o domínio não seja encontrado, definimos o status como inativo.
+        if ($status === null) {
+            $status = "inativo";
+        }
+
+        // Retornar a view com o status do domínio
+        return view('dominio', ['dominio' => $dominio, 'status' => $status]);
     }
 }
--- resources/views/dominio.blade.php
+++ resources/views/dominio.blade.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View domínio</title>
+    <title>$dominio</title>
 </head>
 <body>
     Você está na <b>view</b> de <b>dominio</b>
+
+    O domínio $dominio está $status
 </body>
 </html>
