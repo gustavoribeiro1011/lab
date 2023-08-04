d264be2beb840c44e9e890035f0ab5b7f2cd9339
Gustavo Ribeiro <gustavoribeiro1011@gmail.com>
2023-08-04 18:55:58 +0000

    lab

--- resources/views/clients/edit.blade.php
+++ resources/views/clients/edit.blade.php
@@ -10,15 +10,15 @@
     @csrf
     <div class="mb-3">
         <label for="nome" class="form-label">Nome</label>
-        <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite o nome">
+        <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite o nome" value="{{ $client->nome }}">
     </div>
     <div class="mb-3">
         <label for="endereco" class="form-label">Endereço</label>
-        <input type="text" class="form-control" id="endereco" name="endereco" placeholder="Digite o endereço">
+        <input type="text" class="form-control" id="endereco" name="endereco" placeholder="Digite o endereço" value="{{ $client->endereco }}">
     </div>
     <div class="mb-3">
         <label for="observacao" class="form-label">Observação</label>
-        <textarea class="form-control" id="observacao" name="observacao" placeholder="Digite a observação"></textarea>
+        <textarea class="form-control" id="observacao" name="observacao" placeholder="Digite a observação" value="{{ $client->observacao }}"></textarea>
     </div>
     <button type="submit" class="btn btn-success">Enviar</button>
 </form>
