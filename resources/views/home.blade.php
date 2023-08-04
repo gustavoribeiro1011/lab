4541845834d2110e94692169b4f357df22d48e2c
Gustavo Ribeiro <gustavoribeiro1011@gmail.com>
2023-08-04 21:19:10 +0000

    lab

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Olá, eu sou a página home.
-</body>
-</html>
+@extends('app')
+
+@section('titulo', 'Home')
+
+@section('conteudo')
+Olá, eu sou a página home.
+@endsection
