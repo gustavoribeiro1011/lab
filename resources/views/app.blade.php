075e4c62be6e670ed12e48d04b96b4d822a6bdb8
Gustavo Ribeiro <gustavoribeiro1011@gmail.com>
2023-08-04 21:04:33 +0000

    lab

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,9 +9,22 @@
 </head>
 
 <body>
-    <nav class="navbar bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('index') }}">Lab</a>
+            <a class="navbar-brand" href="#">Lab</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('index') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('clients.index') }}">Clientes</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
     <div class="container">
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::get('/', function () {
-//     return view('welcome');
+//     return view('e');
 // });
 
 Route::get('/', [SiteController::class, 'index'])->name('index');
