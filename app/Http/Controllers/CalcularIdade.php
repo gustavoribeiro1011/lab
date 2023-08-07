c2a198dcb8e9a8f240a0137194ab0136f561e79c
Gustavo Ribeiro <gustavoribeiro1011@gmail.com>
2023-08-07 12:20:32 +0000

    lab

--- app/Http/Controllers/CalcularIdade.php
+++ app/Http/Controllers/CalcularIdade.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class CalcularIdade extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        echo "teste";
+
+        $idade = null;
+
+        if($request->isMethod('post')){
+
+            //Resgatar a data de nascimento preenchido no formulário
+            $dataNascimento = $request->input("data_nascimento");
+
+            //Definir a data atual
+            $dataAtual = Carbon::now();
+
+            //Converter a data de nascimento para objeto carbon
+            $dataNasc = Carbon::createFromFormat("d/m/Y",$dataNascimento);
+
+            //Calcular a diferença de anos entre data de nascimento e data atual
+            $idade = $dataAtual->diffInYears($dataNascimento);
+        }
+
+        return view('calcular-idade', ['idade' => $idade]);
     }
 }
---
+++ resources/views/exemplos/calcular-idade.blade.php
@@ -0,0 +1,33 @@
+</html>
+
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Calcular Idade</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+
+<body>
+
+    @if ($idade != null)
+    <p>Idade: {{ $idade }}</p>
+    @endif
+
+    <form action="{{ route('calcular-idade') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="data_nascimento" class="form-label">Data de Nascimento</label>
+            <input type="text" class="form-control" id="data_nascimento" name="data_nascimento" placeholder="dd/mm/aaaa">
+        </div>
+        <button type="submit" class="btn btn-success">Calcular Idade</button>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
