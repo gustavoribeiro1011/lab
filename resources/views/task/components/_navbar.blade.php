ad282cc25e4c384fc60aa111c28a76177d8b86e8
Gustavo Ribeiro <gustavoribeiro1011@gmail.com>
2023-08-08 13:55:42 +0000

    lab

--- resources/views/task/components/_navbar.blade.php
+++ resources/views/task/components/_navbar.blade.php
@@ -1,6 +1,6 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Task</a>
+            <a class="navbar-brand" href="{{ route('task') }}">Task</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -8,9 +8,19 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="{{ route('task') }}">Home</a>
                     </li>
                 </ul>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Tarefas
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="{{ route('task.create') }}">Nova tarefa</a></li>
+                        <li><a class="dropdown-item" href="#">Ver todas</a></li>                        
+                    </ul>
+                </li>
             </div>
         </div>
     </nav>
