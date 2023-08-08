40ca8b19b40a72e26f3b86dfb4be8dac166beec2
Gustavo Ribeiro <gustavoribeiro1011@gmail.com>
2023-08-08 14:11:38 +0000

    lab

--- resources/views/task/components/_navbar.blade.php
+++ resources/views/task/components/_navbar.blade.php
@@ -1,36 +1,42 @@
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/task">Task</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/task">Home</a>
-                    </li>
-                </ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/task">Task</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/task">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                         data-bs-toggle="dropdown" aria-expanded="false">
                         Tarefas
                     </a>
+                    @php
+                        $itensMenu = [
+                            [
+                                'descricao' => 'Nova tarefa',
+                                'link' => route('task.create'),
+                            ],
+                        ];
+                    @endphp
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        @php
-                            $itensMenu = [
-                                [
-                                    'descricao' => 'Nova tarefa',
-                                    'link' => route('task.create'),
-                                ],
-                            ];
-                        @endphp
-
                         <li><a class="dropdown-item" href="#">Nova tarefa</a></li>
-
                         <li><a class="dropdown-item" href="#">Ver todas</a></li>
                     </ul>
                 </li>
-            </div>
+
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
         </div>
-    </nav>
+    </div>
+</nav>
