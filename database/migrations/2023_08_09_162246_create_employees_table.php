5a1501f4f3b1b4866a25a7af74557e4e94bc1267
Gustavo Ribeiro <gustavoribeiro1011@gmail.com>
2023-08-10 13:23:15 +0000

    lab

--- database/migrations/2023_08_09_162246_create_employees_table.php
+++ database/migrations/2023_08_09_162246_create_employees_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->date('data_demissao')->nullable();
             $table->timestamps();
         });
-    }
+    
 
     /**
      * Reverse the migrations.
