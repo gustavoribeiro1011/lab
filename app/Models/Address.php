572534dba8f3c3b0675a2b465c49d0b334adc2c9
Gustavo Ribeiro <gustavoribeiro1011@gmail.com>
2023-08-10 17:41:36 +0000

    lab

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['logradouro','numero','bairro','cidade','complemento','cep','estado'];
+
+    /**
+     * Defini que o Endereço pertence ao Funcionário
+     *
+     * @return void
+     */
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
